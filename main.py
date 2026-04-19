305222bb70dbd82747779127b2efaf7d406f1105
Parth Biru <your@email.com>
2026-04-19 14:37:42 +0530

    Add FastAPI project - learning started

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+
+    return data
+
+
 @app.get("/")
 def hello():
-    return {"message": "Hello, World!"}
+    return {"message": "patients managment system API"}
 
 @app.get("/about")
 def about():
-    return {"message": "This is the about page!"}
+    return {"message": "A fully functional API to manage your patients records"}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
